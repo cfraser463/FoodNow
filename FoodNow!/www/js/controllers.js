5510092471a0d75703b3f77ec57111654d3f5608
cfraser463 <cfraser463@gmail.com>
2017-03-08 22:58:35 -0400

    Initial Ionic Project
    Also include order confirmation funtion

---
+++ FoodNow!/www/js/controllers.js
@@ -0,0 +1,57 @@
+angular.module('app.controllers',  [])
+  
+.controller('restaurantsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('foodNowCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('foodCartCtrl', ['$scope', '$stateParams', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $ionicPopup) {
+    
+ // A confirm dialog
+ $scope.showConfirm = function() {
+   var confirmPopup = $ionicPopup.confirm({
+     title: 'Confirm Order',
+     template: 'Are you sure that this is what you want?'
+   });
+
+   confirmPopup.then(function(res) {
+     if(res) {
+       console.log('You are sure');
+     } else {
+       console.log('You are not sure');
+     }
+   });
+ };
+    
+}])
+   
+.controller('cloudTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('kFCMenuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ FoodNow!/www/js/routes.js
@@ -0,0 +1,80 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      /* 
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.foodNow'
+      2) Using $state.go programatically:
+        $state.go('tabsController.foodNow');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab1/page2
+      /page1/tab2/page2
+  */
+  .state('tabsController.foodNow', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/foodNow.html',
+        controller: 'foodNowCtrl'
+      },
+      'tab2': {
+        templateUrl: 'templates/foodNow.html',
+        controller: 'foodNowCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.foodCart', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/foodCart.html',
+        controller: 'foodCartCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cloudTabDefaultPage', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/cloudTabDefaultPage.html',
+        controller: 'cloudTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.kFCMenu', {
+    url: '/page5',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/kFCMenu.html',
+        controller: 'kFCMenuCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/tab1/page2')
+
+  
+
+});
